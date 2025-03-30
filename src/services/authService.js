686c4d47a7cf795cc6924aba668db2e151eff851
Binod-Perera <108719707+BinodPerera@users.noreply.github.com>
2025-03-30 12:30:27 +0530

    debugging

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,5 @@
 
 const API_URL = `${process.env.REACT_APP_API_URL}/api/auth`;
-// const API_URL = "http://localhost:5001/api/auth";
 
 export const loginUser = async ( email, password) => {
     try {
